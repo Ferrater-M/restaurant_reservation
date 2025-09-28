ed001f4d75097b25450601ccaaa23a49f579e1c7
joanaxyz <joanacarlagako15@gmail.com>
2025-09-28 17:00:13 +0800

    setup(rr_project): add requirements, env and settings

--- rr_project/rr_project/settings.py
+++ rr_project/rr_project/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 from decouple import config
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -85,10 +86,9 @@ WSGI_APPLICATION = 'rr_project.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(
+        "postgresql://postgres.musaoslrzfsrvujbycyb:hgdkgasdvcxadr@aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres"
+    )
 }
 
 
